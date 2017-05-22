bf33d480492844f14333804e72514c98df6f4517
Virginie3377 <virginiecapdevielle77@gmail.com>
2017-05-22 10:49:21 +0200

    pour les series : Mise en forme + bouton modifier ok

--- src/WCS/TvShowManagerBundle/Controller/TvShowController.php
+++ src/WCS/TvShowManagerBundle/Controller/TvShowController.php
@@ -27,10 +27,9 @@ class TvShowController extends Controller
             ->add('type', TextType::class)
             ->add('url', TextType::class)
             ->add('year', TextType::class)
-            ->add('save', SubmitType::class)
+            ->add('Enregistrer', SubmitType::class)
             ->getForm()
         ;
-
 // Hydrate notre objet avec la donnée du formulaire
         $form->handleRequest($request);
         // Si method POST et si le form est valid
@@ -40,10 +39,7 @@ class TvShowController extends Controller
             $em->flush();
             return $this->redirectToRoute('wcs_tv_show_manager_index');
         }
-
-
 // createView() permet à la vue d’afficher le formulaire
-
         return $this->render('WCSTvShowManagerBundle:TvShow:add.html.twig', array(
             'tvShow' => $tvShow,
             'my_form' => $form->createView(),
@@ -55,14 +51,39 @@ class TvShowController extends Controller
         $em = $this->getDoctrine()->getManager();
         $tvShow = $em->getRepository('WCSTvShowManagerBundle:TvShow')->find($id);
 
-
-
-
         return $this->render('WCSTvShowManagerBundle:TvShow:show.html.twig', array(
             'tvShow' => $tvShow,
 
-
         ));
+    }
+
+    public function modifyAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $tvShow = $em->getRepository('WCSTvShowManagerBundle:TvShow')->find($id);
+        $form = $this->createFormBuilder($tvShow)
+            ->add('name', TextType::class)
+            ->add('type', TextType::class)
+            ->add('url', TextType::class)
+            ->add('year', TextType::class)
+            ->add('Enregistrer', SubmitType::class)
+            ->getForm()
+            ;
+
+        $form->handleRequest($request);
+        // Si method POST et si le form est valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+            return $this->redirectToRoute('wcs_tv_show_manager_index');
+        }
+        // createView() permet à la vue d’afficher le formulaire
+        return $this->render('WCSTvShowManagerBundle:TvShow:modify.html.twig', array(
+            'tvShow' => $tvShow,
+            'my_form' => $form->createView(),
+        ));
+
     }
 
     public function deleteAction($id)
